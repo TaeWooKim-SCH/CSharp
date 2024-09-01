dab9b9ef41c9f49e724413c061afd3427999a1e5
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-01 16:59:43 +0900

    add: 객체 지향(복사, 참조)

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,9 +5,18 @@
     // Knight
     // 속성: hp, attack, pos
     // 기능: Move, Attack, Die
+
+    // Ref 참조
     class Knight {
         public int hp; // public이 없으면 해당 클래스 내부에서만 사용한다는 의미
         public int attack;
+
+        public Knight Clone() {
+            Knight knight = new Knight();
+            knight.hp = hp;
+            knight.attack = attack;
+            return knight;
+        }
 
         public void Move() {
             Console.WriteLine("Knight Move");
@@ -16,14 +25,36 @@
             Console.WriteLine("Knight Attack");
         }
     }
+
+    // Copy 복사
+    struct Mage {
+        public int hp;
+        public int attack;
+    }
     internal class Program {
+        static void KillMage(Mage mage) {
+            mage.hp = 0;
+        }
+
+        static void KillKnight(Knight knight) {
+            knight.hp = 0;
+        }
+
         static void Main(string[] args) {
+            Mage mage;
+            mage.hp = 100;
+            mage.attack = 50;
+            KillMage(mage); // hp = 100
+
+            Mage mage2 = mage;
+            mage2.hp = 0;
+
             Knight knight = new Knight();
             knight.hp = 100;
             knight.attack = 10;
 
-            knight.Move();
-            knight.Attack();
+            Knight knight2 = knight.Clone();
+            knight2.hp = 0;
         }
     }
 }
