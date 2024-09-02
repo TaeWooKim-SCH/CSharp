3534c134f3e1e941e71f34e57d51e2fe3192cf2c
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-02 18:09:12 +0900

    add: static의 정체

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,24 +8,30 @@
 
     // Ref 참조
     class Knight {
+        static public int counter; // 오로지 1개만 존재! -> 각 다른 객체들이 모두 공유해서 하나만 사용한다!
+
+        public int id;
         public int hp; // public이 없으면 해당 클래스 내부에서만 사용한다는 의미
         public int attack;
 
+        static public void Test() {
+            counter++;
+        }
+
+        static public Knight CreateKnight() {
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 1;
+            return knight;
+        }
+
         public Knight () {
+            id = counter;
+            counter++;
+
             hp = 100;
             attack = 0;
             Console.WriteLine("생성자 호출");
-        }
-
-        public Knight(int hp): this() {
-            this.hp = hp;
-            Console.WriteLine("int 생성자 호출!");
-        }
-
-        public Knight(int hp, int attack) {
-            this.hp = hp;
-            this.attack = attack;
-            Console.WriteLine("int, int 생성자 호출!");
         }
 
         public Knight Clone() {
@@ -46,8 +52,13 @@
     internal class Program {
 
         static void Main(string[] args) {
-            Knight knight = new Knight(50, 5);
-            
+            Knight knight = Knight.CreateKnight(); // static
+            knight.Move(); // 일반
+
+            Console.WriteLine();
+
+            Random rand = new Random();
+            rand.Next(0, 2);
         }
     }
 }
