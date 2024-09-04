bcfb0f765e5d9a6c52b6f0e99434c33da8ed4d62
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-04 23:54:22 +0900

    add: 클래스 형식 변환

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,31 +1,36 @@
 ﻿namespace CSharp
 {
     // OOP(은닉성/상속성/다형성)
-    // 자동차
-    // 핸들 페달 차문
-        // 전기장치 엔진 기름 <-> 외부 노출
-
-    class Knight {
-        // 접근 한정자(default = private)
-        // public protected private
-        // public: 모두 접근 가능
-        // private: 해당 클래스 내에서만 접근 가능
-        // protected: 상속을 받은 클래스는 접근 가능
-        protected int hp;
-        
-    }
-
-    class SuperKnight: Knight {
-        void Test() {
-            hp = 10;
-        }
-    }
     
-    internal class Program {
+    class Player {
+        protected int hp;
+        protected int attack;
+    }
 
+    class Knight: Player {
+
+    }
+
+    class Mage: Player {
+        public int mp;
+    }
+
+    internal class Program {
+        static void EnterGame(Player player) {
+            Mage mage = player as Mage;
+            if (mage != null) {
+                mage.mp = 10;
+                return;
+            } else {
+                Console.WriteLine("끝");
+                return;
+            }
+        }
         static void Main(string[] args) {
             Knight knight = new Knight();
-            // knight.hp = 10; -> 접근 불가
+            Mage mage = new Mage();
+
+            EnterGame(knight);
         }
     }
 }
