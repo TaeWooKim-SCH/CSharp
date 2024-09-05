f264ae0c0b7eb0d97a1ac81980c3df48df05483a
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-05 14:18:42 +0900

    add: 문자열 둘러보기

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,8 +6,18 @@
 
             // 1. 찾기
             bool found = name.Contains("Harry");
-            int index = name.IndexOf('P');
-            
+            int index = name.IndexOf('z'); // -1
+
+            // 2. 변형
+            name = name + " Junior";
+
+            string lowerCase = name.ToLower();
+            string upperCase = name.ToUpper();
+            string newName = name.Replace('r', 'l');
+
+            // 3. 분할
+            string[] names = name.Split(new char[] { ' ' });
+            string substringName = name.Substring(5);
         }
     }
 }
