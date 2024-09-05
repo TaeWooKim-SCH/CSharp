63ceb32af3c3d603b54bfc111a4d838d110c8770
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-05 14:55:49 +0900

    add: TextRPG2 몬스터 생성

---
+++ CSharp/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp {
+    public enum PlayerType {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+
+    class Player: Creature {
+        protected PlayerType type = PlayerType.None;
+
+        protected Player(PlayerType type): base(CreatureType.Player) {
+            this.type = type;
+        }
+
+        public PlayerType GetPlayerType() { return type; }
+    }
+
+    class Knight: Player {
+        public Knight(): base(PlayerType.Knight) {
+            SetInfo(100, 10);
+        }
+
+    }
+
+    class Archer: Player {
+        Archer() : base(PlayerType.Archer) {
+            SetInfo(75, 10);
+        }
+    }
+
+    class Mage: Player {
+        Mage() : base(PlayerType.Mage) {
+            SetInfo(50, 15);
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -2,22 +2,11 @@
 {
     internal class Program {
         static void Main(string[] args) {
-            string name = "Harry Potter";
+            Player player = new Knight();
+            Monster monster = new Orc();
 
-            // 1. 찾기
-            bool found = name.Contains("Harry");
-            int index = name.IndexOf('z'); // -1
-
-            // 2. 변형
-            name = name + " Junior";
-
-            string lowerCase = name.ToLower();
-            string upperCase = name.ToUpper();
-            string newName = name.Replace('r', 'l');
-
-            // 3. 분할
-            string[] names = name.Split(new char[] { ' ' });
-            string substringName = name.Substring(5);
+            int damage = player.GetAttack();
+            monster.OnDamaged(damage);
         }
     }
 }
