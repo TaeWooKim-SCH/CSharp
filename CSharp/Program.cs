d7e0cf3d1fc0efedf881584be918f30eadb78a50
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-05 14:07:20 +0900

    add: 다형성

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,23 +1,43 @@
 ﻿namespace CSharp
 {
     // OOP(은닉성/상속성/다형성)
+    // 다형성
+    // 오버라이딩: 어떤 함수에 실제 타입에 따라 다양한 형태로 동작하도록 하는 것
     
     class Player {
         protected int hp;
         protected int attack;
+
+        public virtual void Move() {
+            Console.WriteLine("Player 이동!");
+        }
     }
 
     class Knight: Player {
-
+        public override void Move() {
+            base.Move();
+            Console.WriteLine("Knight 이동!");
+        }
     }
 
     class Mage: Player {
         public int mp;
+
+        public override void Move() {
+            Console.WriteLine("Mage 이동!");
+        }
+    }
+
+    class SuperKnight: Knight {
+        public override void Move() {
+            Console.WriteLine("SuperKnight 이동!");
+        }
     }
 
     internal class Program {
         static void EnterGame(Player player) {
-            Mage mage = player as Mage;
+            player.Move();
+            Mage mage = (player as Mage);
             if (mage != null) {
                 mage.mp = 10;
                 Console.WriteLine("mp 변경됨");
@@ -26,11 +46,12 @@
                 Console.WriteLine("끝");
                 return;
             }
+
         }
         static void Main(string[] args) {
             Knight knight = new Knight();
             Mage mage = new Mage();
-
+            knight.Move();
             EnterGame(mage);
         }
     }
