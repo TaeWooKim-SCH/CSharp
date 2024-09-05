1f348817a21f4e124ec2bc27bad7f90563c321e6
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-05 17:03:41 +0900

    add: 배열

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,11 +1,24 @@
 ﻿namespace CSharp
 {
-    internal class Program {
-        static void Main(string[] args) {
-            Game game = new Game();
+    class Player { 
+    
+    }
 
-            while (true) {
-                game.Process();
+    class Monster {
+
+    internal class Program {
+        }
+
+        static void Main(string[] args) {
+            // 배열
+            int[] scores = { 10, 20, 30, 40, 50 };
+
+            //for (int i = 0; i < 5; i++) {
+            //    Console.WriteLine(scores[i]);
+            //}
+
+            foreach (int score in scores) {
+                Console.WriteLine(score);
             }
         }
     }
